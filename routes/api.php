f71c6ce621130c83e7df245981ff7c636b30c006
elamboatonge <elamboatonge@gmail.com>
2021-10-08 10:44:39 +0300

    adds rental migration

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Query data
+Route::get('/{name?}/{bedroom?}/{bathroom?}/{storey?}/{garage?}/{startPrice?}/{endPrice?}', 'RentalController@index');
